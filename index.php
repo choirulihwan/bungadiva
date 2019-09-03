771a4966533db12f4326f6b181ac7ba2b62a36ea
choirulihwan <aristhu_oracle@yahoo.com>
2019-09-03 15:11:32 +0700

    Code Cleaning

--- index.php
+++ index.php
@@ -92,26 +92,13 @@ $cmbKat = render_combo($arrKategori, 'kategori', $KatSelected, "", "", 'comboKat
         </div><!--/.navbar-collapse -->
       </div>
     </nav>
-
-	<!--<div class="jumbotron">
-      <div class="container" style="text-align:center;">
-        <h1>Bunga Diva</h1>
-        <p>Gambar disini adalah semua stock. Silahkan pilih status untuk melihat stock yang ready</p>
-        <p><a class="btn btn-primary btn-lg" href="#" role="button">Learn more &raquo;</a></p>
-      </div>
-    </div>-->
-
-<div class="container gallery-container">    
-    
-    
+	
+<div class="container gallery-container"> 
     <div class="tz-gallery">
-
         <div class="row scroll">
 		
 		<?php
-			
 			for($i=0;$i<count($bunga);$i++){
-				
 		?>
 			<div class="col-sm-6 col-md-4">
                 <div class="thumbnail">
@@ -134,85 +121,9 @@ $cmbKat = render_combo($arrKategori, 'kategori', $KatSelected, "", "", 'comboKat
 		<?php
 			}
 		?>
-            <!--
-			<div class="col-sm-6 col-md-4">
-                <div class="thumbnail">
-                    <a class="lightbox" href="images/park.jpg">
-                        <img src="images/park.jpg" alt="Park">
-                    </a>
-                    <div class="caption">
-                        <h3>Thumbnail label</h3>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-                    </div>
-                </div>
-            </div>
-            <div class="col-sm-6 col-md-4">
-                <div class="thumbnail">
-                    <a class="lightbox" href="images/bridge.jpg">
-                        <img src="images/bridge.jpg" alt="Bridge">
-                    </a>
-                    <div class="caption">
-                        <h3>Thumbnail label</h3>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-                    </div>
-                </div>
-            </div>
-            <div class="col-sm-6 col-md-4">
-                <div class="thumbnail">
-                    <a class="lightbox" href="images/tunnel.jpg">
-                        <img src="images/tunnel.jpg" alt="Tunnel">
-                    </a>
-                    <div class="caption">
-                        <h3>Thumbnail label</h3>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-                    </div>
-                </div>
-            </div>
-			
-			<div class="col-sm-6 col-md-4">
-                <div class="thumbnail">
-                    <a class="lightbox" href="images/coast.jpg">
-                        <img src="images/coast.jpg" alt="Coast">
-                    </a>
-                    <div class="caption">
-                        <h3>Thumbnail label</h3>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-                    </div>
-                </div>
-            </div>
-            <div class="col-sm-6 col-md-4">
-                <div class="thumbnail">
-                    <a class="lightbox" href="images/rails.jpg">
-                        <img src="images/rails.jpg" alt="Rails">
-                    </a>
-                    <div class="caption">
-                        <h3>Thumbnail label</h3>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-                    </div>
-                </div>
-            </div>
-            <div class="col-sm-6 col-md-4">
-                <div class="thumbnail">
-                    <a class="lightbox" href="images/traffic.jpg">
-                        <img src="images/traffic.jpg" alt="Traffic">
-                    </a>
-                    <div class="caption">
-                        <h3>Talenan Hias Bunga Rumput</h3>
-                        <p style="text-align:left">
-Full cat deco + kain goni dipermanis dengan lace kupu-kupu. Ukuran: -/+ 15cm x 30cm</p>
-						<p style="margin-top:10px;"><button type='button' class='btn btn-lg btn-success'><i class='fa fa-whatsapp fa-2x'></i></button></p>
-                    </div>
-                </div>
-            </div>-->
-			
-			
             <a href="index2.php?page=<?=$page?>&jml=<?=$jml_bunga?>&status=<?=$status?>&kategori=<?=$kategori?>">next</a>
-				
         </div>
-
     </div>
-	
-	
 
 </div>
 
