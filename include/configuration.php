4ab57f4c514e2e5fa0abfa982f7c82e8b7083aa5
choirulihwan <aristhu_oracle@yahoo.com>
2019-09-03 15:05:44 +0700

    remove line in config.php

--- include/configuration.php
+++ include/configuration.php
@@ -2,9 +2,9 @@
 
 //application
 $db['trans']['db_host'] = 'localhost';
-$db['trans']['db_user'] = 'root';
+$db['trans']['db_user'] = '';
 $db['trans']['db_password'] = '';
-$db['trans']['db_name'] = 'db_bungadiva';
+$db['trans']['db_name'] = '';
 
 
 
